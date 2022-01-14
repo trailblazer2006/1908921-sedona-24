9706695d54271a057b42533ce3ba40b602d54126
Alexander Senko <alexander.senko@forsta.com>
2022-01-14 10:37:03 +0300

    fix: fix according to the code review use button for volume knob use button for likes in gallery fix tablet image for likes button

--- source/js/app.js
+++ source/js/app.js
@@ -11,3 +11,16 @@ btnClose.addEventListener('click', function () {
   mainNav.classList.remove('main-nav--opened');
   mainNav.classList.add('main-nav--closed');
 });
+
+const btnLikesAll = document.querySelectorAll('.gallery__likes-button')
+btnLikesAll.forEach(function (btnLikes) {
+  btnLikes.addEventListener('click', function (evt) {
+    const numberSpan = evt.target.nextElementSibling;
+    if (numberSpan) {
+      const num = parseInt(numberSpan.innerText, 10);
+      if (!isNaN(num)) {
+        numberSpan.innerText = String(num + 1);
+      }
+    }
+  });
+});
